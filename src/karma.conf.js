ccef0b16412639c23e1b9886bb51ff84bd43284f
ngako <laurent.ngako@gmail.com>
2016-10-31 22:34:07 +0100

    Update path of the transpiled app js and map file

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,7 +1,7 @@
 // #docregion
 module.exports = function(config) {
 
-  var appBase    = 'app/';       // transpiled app JS and map files
+  var appBase    = 'build/app/';       // transpiled app JS and map files
   var appSrcBase = 'app/';       // app source TS files
   var appAssets  = 'base/app/'; // component assets fetched by Angular's compiler
 
