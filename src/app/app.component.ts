1b42449f586966b0f359127845155f0a87f14626
ngako <laurent.ngako@gmail.com>
2016-11-01 01:36:15 +0100

    Add component function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,4 +4,26 @@ import { Component } from '@angular/core';
     selector: 'my-app',
     templateUrl: "/app/app.component.tpl.html"
 })
-export class AppComponent { }
+export class AppComponent {
+    operation:string;
+    firstvalue:number = 0;
+    scdvalue:number = 0;
+    result:number = 0;
+    listOperation: Array<{firstvalue:number,scdvalue:number,operation:string}>;
+
+    process(operation:string, firstvalue:number, scdvalue:number):void{
+
+    }
+
+    addition():void{
+
+    }
+
+    multiplication(): void{
+
+    }
+
+    substration(): void{
+
+    }
+ }
