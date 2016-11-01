8501fd0b632f68ed23bb60bf893b3ef3393a1673
ngako <laurent.ngako@gmail.com>
2016-11-01 01:22:53 +0100

    Use full path of templateUrl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: "tpl/app.component.tpl.html"
+    templateUrl: "/app/app.component.tpl.html"
 })
 export class AppComponent { }
